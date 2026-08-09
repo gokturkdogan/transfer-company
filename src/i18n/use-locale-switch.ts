57b7573415554b8b431ad3025e51e7fef9a6d22c
Göktürk DOĞAN <gokturk.dogan@hotmail.com>
2026-08-09 18:48:22 +0300

    deploy fix

--- src/i18n/use-locale-switch.ts
+++ src/i18n/use-locale-switch.ts
@@ -1,21 +1,22 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
 import { useCallback } from "react";
 
 import { usePathname, useRouter } from "@/i18n/navigation";
 
 export function useLocaleSwitch() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
   const router = useRouter();
 
   return useCallback(
     (locale: string) => {
-      const query = searchParams.toString();
+      const query =
+        typeof window !== "undefined"
+          ? window.location.search.replace(/^\?/, "")
+          : "";
       const href = query ? `${pathname}?${query}` : pathname;
       router.replace(href, { locale });
     },
-    [pathname, router, searchParams],
+    [pathname, router],
   );
 }
